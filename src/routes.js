e2b9f066c1c85d93f57daa176b0848ce947fd7b6
Renan Ramos <renanramos97@hotmail.com>
2020-04-28 21:04:31 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const PokemonController = require('./controllers/PokemonController');
+
+const routes = express.Router();
+
+/**
+ * Definição de rotas da aplicação aqui.
+ */
+
+module.exports(routes);
