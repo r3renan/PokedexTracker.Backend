d0a513ab311d753dae7883a8511693eb5aaf9104
Renan Ramos <renanramos97@hotmail.com>
2020-05-01 00:35:27 -0300

    createPokemonSeed endpoint now returns different seeds per pokémon generation.

--- src/api/PokemonController.js
+++ src/api/PokemonController.js
@@ -1,31 +1,43 @@
-const connection = require('../database/connection');
 const P = require('../services/pokeApi');
 
 module.exports = {
 
     async createPokemonSeed(req, res) {
-        var promisesArray = [];
-        var resultado = [];
+        const { generation } = req.params;
 
-        for(let i = 1; i <= 151; i++){
-            promisesArray.push(P.getPokemonByName(i));
+        var promisesArray = [];
+        var seed = [];
+
+        switch(generation){
+            case "1":
+                for(let i = 1; i <= 151; i++)
+                    promisesArray.push(P.getPokemonByName(i));
+                break;
+            case "2":
+                for(let i = 152; i <= 251; i++)
+                    promisesArray.push(P.getPokemonByName(i));
+                break;
+            case "3":
+                for(let i = 252; i <= 386; i++)
+                    promisesArray.push(P.getPokemonByName(i));
+                break;
         }
 
         Promise.all(promisesArray)
             .then(result => {
                 
                 result.forEach(function(registro){
-                    resultado.push({
+                    seed.push({
                         "id" : registro.id,
                         "name" : registro.name,
                         "front-default"  : registro.sprites.front_default
                     })
                 });
 
-                return res.json(resultado);
+                return res.json(seed);
             })
             .catch(error => {
-                console.log("Erro na função getPokemonByName: ", error);
+                return res.status(500).json({ Erro: `Comunicação interrompida na requisição ${error.config.url}`})
             })
     }
 };
--- src/api/routes.js
+++ src/api/routes.js
@@ -3,6 +3,6 @@ const PokemonController = require('./PokemonController');
 
 const routes = express.Router();
 
-routes.get('/admin/createPokemonSeed', PokemonController.createPokemonSeed);
+routes.get('/admin/createPokemonSeed/:generation', PokemonController.createPokemonSeed);
 
 module.exports = routes;
