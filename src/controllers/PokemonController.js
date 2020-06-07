303c2bcfaa62d16d4628bc00ae3de35dbc0f5da5
muzzyrella <muzzyrella09@gmail.com>
2020-06-08 02:39:19 +0900

    findPokemon test route now working

--- src/controllers/PokemonController.js
+++ src/controllers/PokemonController.js
@@ -1,17 +1,20 @@
 const models = require("../database/models");
 const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
 
 module.exports = {
     async findPokemon(req, res) {
-        const pokemon = await models.pokemon.findOne({
-            where: {
-                [Op.or]: [
-                    { pokemonId: req.params.pokemon },
-                    { name: req.params.pokemon },
-                ],
-            },
+        const isId = Number.isInteger(req.params.pokemon);
+        const pokemon = await models.Pokemon.findOne({
+            where: isId
+                ? { id: req.params.pokemon }
+                : { name: req.params.pokemon },
         });
-        return pokemon ? res.json(pokemon) : res.send("Pokemon nao encontrado");
+        if (pokemon) {
+            const sprites = await models.Sprite.findOne({
+                where: { PokemonId: pokemon.id },
+            });
+            return res.json({ pokemon, sprites });
+        }
+        return res.send("Pokemon nao encontrado");
     },
 };
